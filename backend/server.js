4a197eb3b64467078f4f45a9ecdbe56a5a06c26e
Nick Mertz <nick.mertz@gmail.com>
2022-02-17 07:08:36 -0600

    MERN Project Start, API & Backend Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log('server...');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+// Express initialization
+const app = express();
+
+// Express Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Root Route
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Welcome to the Support Desk API'});
+});
+
+// Server Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Custom Middleware - Error Handler
+app.use(errorHandler);
+
+// Listen for requests
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
